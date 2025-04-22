10c3222bcadc5d5b3cc7cc8d8bcb4d118b4e4ef9
Jduopu <Jduopu3112@gmail.com>
2025-04-22 11:27:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+"""
+The main handler for the project
+Jon Duopu
+"""
 
+# VARIABLES
+import request
+
+API_KEY = ""
+BASE_URL = ""
+
+# FUNCTIONS
+def get_movie_genre(genre):
+  
+
+def get_movie_details(id):
+  
+
+# FUNCTION CALLS
